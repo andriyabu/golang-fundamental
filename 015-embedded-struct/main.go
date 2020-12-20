79133bbd94fc7a819dc4a5dbcac6a409fc5b2fc2
Andri Yabu <andriyabu@gmail.com>
2020-12-20 18:47:36 +0700

    create displayGroup function

--- 015-embedded-struct/main.go
+++ 015-embedded-struct/main.go
@@ -36,9 +36,21 @@ func main() {
 	group := Group{"Gamer", user, users, true}
 
 	fmt.Println(group.Name, group.Admin.firstName, "Group members : ", group.Users[0].firstName, " and ", group.Users[1].firstName, ", is available: ", group.IsAvailable)
+	fmt.Println("====================================")
+	displayGroup(group)
 
 }
 
-func displayGroup(user Group) {
-
+func displayGroup(group Group) {
+	fmt.Printf("Group Name: %s", group.Name)
+	fmt.Println("")
+	fmt.Printf("Group Admin: %s", group.Admin.firstName)
+	fmt.Println("")
+	fmt.Printf("Group members : %d", len(group.Users))
+	fmt.Println("")
+	for _, user := range group.Users {
+		fmt.Println(user.firstName)
+	}
+	fmt.Printf("Is Available : %t", group.IsAvailable)
+	fmt.Println("")
 }
