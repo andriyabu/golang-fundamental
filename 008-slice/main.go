87e20a76bd1757b7a95357fce4c526ff48cda468
Andri Yabu <andriyabu@gmail.com>
2020-12-20 14:06:54 +0700

    map

---
+++ 008-slice/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// cara mendeklarasikan slice dan langsung mengisinya
+	fruits := []string{"apple", "banana", "orange", "watermelon"}
+
+	// cara deklarasi ke dua slice kosong untuk mengisi dengan data dinamis menggunakan append
+	// var fruits []string
+
+	fruits = append(fruits, "coconut")
+	fruits = append(fruits, "melon")
+	fruits = append(fruits, "pinutes")
+
+	for _, fruit := range fruits {
+		fmt.Println(fruit)
+	}
+}
