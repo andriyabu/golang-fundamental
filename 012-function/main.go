6450570b807c10a24063f0ea1e1b9f8e78999801
Andri Yabu <andriyabu@gmail.com>
2020-12-20 15:37:32 +0700

    function declaration with two parameters

--- 012-function/main.go
+++ 012-function/main.go
@@ -7,9 +7,16 @@ import (
 func main()  {
 	result := hello("Saya sedang")
 	fmt.Println(result)
+
+	addresult := add(5,5)
+	fmt.Println(addresult)
 }
 
 func hello(sentence string) string  {
 	mySentence := sentence + " Golang Fundamental"
 	return mySentence
+}
+
+func add(number, numberTwo int) int  {
+	return number + numberTwo
 }
