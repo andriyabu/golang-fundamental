089042d4a5430e8825df0c0ee72b9f4a3db38bad
Andri Yabu <andriyabu@gmail.com>
2020-12-20 15:45:36 +0700

    fungsi dengan multiple return

--- 012-function/main.go
+++ 012-function/main.go
@@ -4,19 +4,32 @@ import (
 	"fmt"
 )
 
-func main()  {
+func main() {
 	result := hello("Saya sedang")
 	fmt.Println(result)
 
-	addresult := add(5,5)
+	addresult := add(5, 5)
 	fmt.Println(addresult)
+
+	luas, keliling := calculate(8, 2)
+
+	fmt.Println(luas)
+	fmt.Println(keliling)
 }
 
-func hello(sentence string) string  {
+func hello(sentence string) string {
 	mySentence := sentence + " Golang Fundamental"
 	return mySentence
 }
 
-func add(number, numberTwo int) int  {
+func add(number, numberTwo int) int {
 	return number + numberTwo
+}
+
+// multiple return
+func calculate(panjang, lebar int) (int, int) {
+	luas := panjang * lebar
+	keliling := 2 * (panjang + lebar)
+
+	return luas, keliling
 }
