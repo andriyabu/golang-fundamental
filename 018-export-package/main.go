a1b4d020559a50cd0cd7089fa8a06e32fc33d2a6
Andri Yabu <andriyabu@gmail.com>
2020-12-21 11:45:25 +0700

    fix warning for User struct declaration

--- 018-export-package/main.go
+++ 018-export-package/main.go
@@ -6,16 +6,31 @@ import (
 
 func main() {
 
-	user := management.User{1, "andri", "yabu", "andriyabu@gmail.com", true}
+	user := management.User{
+		ID:        1,
+		FirstName: "andri",
+		LastName:  "yabu",
+		Email:     "andriyabu@gmail.com",
+		IsActive:  true,
+	}
 	user2 := management.User{
+		ID:        2,
 		FirstName: "Abeleon",
 		LastName:  "Godwin Pekambani",
 		Email:     "abeleongodwin@gmail.com",
 		IsActive:  true,
 	}
 
-	users := []management.User{user, user2}
-	group := management.Group{"Gamer", user, users, true}
+	users := []management.User{
+		user,
+		user2,
+	}
+	group := management.Group{
+		Name:        "Gamer",
+		Admin:       user,
+		Users:       users,
+		IsAvailable: true,
+	}
 
 	group.DisplayGroup()
 
