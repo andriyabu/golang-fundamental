a984ae3a1f96ab42b60a4affed68d774382134ef
Andri Yabu <andriyabu@gmail.com>
2020-12-21 12:05:09 +0700

    change variable declaration style

--- 019-pointer/main.go
+++ 019-pointer/main.go
@@ -5,14 +5,18 @@ import (
 )
 
 func main() {
-	numberA := 5
-	numberB := &numberA // numberB diisi dengan alamat memory numberA
+	// deklarasi variable langsung
+	// numberA := 5
+	// numberB := &numberA // numberB diisi dengan alamat memory numberA /referensiasi
+
+	var numberA int = 5
+	var numberB *int = &numberA // numberB diisi dengan alamat memory numberA /referensiasi
 
 	fmt.Println(numberA)  // 5
 	fmt.Println(numberB)  // 0xc00010c000
 	fmt.Println(*numberB) // 5
 
-	*numberB = 10
+	*numberB = 20
 
 	fmt.Println(numberA)  // 10
 	fmt.Println(numberB)  // 0xc00010c000
